a38f5e71619d638ecbe684dc2d1e6b4d378e9082
Pody <daniil2007xboctob@gmail.com>
2025-07-21 19:04:51 +0300

    add docker

---
+++ backend/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [ "$DATABASE" = "postgres" ]
+then
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+      sleep 0.1
+    done
+fi
+uv run manage.py makemigrations
+uv run manage.py migrate
+uv run manage.py createcachetable
+uv run manage.py collectstatic  --noinput
+gunicorn --bind 0.0.0.0:8080 --workers 3 --threads 2 core.wsgi:application
+
+exec "$@"
