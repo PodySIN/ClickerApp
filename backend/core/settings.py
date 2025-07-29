32711dcab817a2d34cf0d42bf9720334322d0902
Pody <daniil2007xboctob@gmail.com>
2025-07-29 14:50:20 +0300

    fix cors

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -32,9 +32,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
-    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
